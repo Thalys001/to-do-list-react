3001962fcb2be53bc0c14943be0e3e9328113bc6
Thalys001 <thalyssouza708@gmail.com>
2023-02-01 20:58:38 -0300

    feat(desktop/add list): creating function that adds the item to the list

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,28 +2,46 @@ import { useState } from 'react'
 import './App.css'
 
 function Todo() {
-  // const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [itemList, setItemList] = useState([])
+
+  function handleChangeInput(event) {
+    const inputTask = event.target.value;
+    setTask(inputTask);
+  }
+
+  function handleAddItemToList(event) {
+    event.preventDefault();
+
+    if (!task) {
+      return
+    };
+
+    setItemList([...itemList, task])
+    setTask("")
+  }
 
   return (
-    <div className="App">
+    <div className="todo-wrapper">
       <h1>TO-DO</h1>
-      <form>
-      <input type="text"  placeholder='Adicone uma tarefa'/>
-      <button>Criar</button>
-      <div className='task-created'>
-      <p> Tarefas criadas <span>0 de 0</span></p>
-      <p>Conclúidas <span>0 de 0</span></p>
-      </div>
+      <form onSubmit={handleAddItemToList}>
+        <input type="text" placeholder='Add new task' onChange={handleChangeInput} value={task} />
+
+        <button type='submit'>Create</button>
       </form>
+
+      <div className='tasks'>
+        <p>Created Tasks<span>0</span></p>
+        <p>Done Tasks<span>0 de 0</span></p>
+      </div>
+
+      <ul className='todo-list'>
+        {itemList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default Todo
-
-{/* <div className="card">
-  <button>Adicionar</button>
-</div>
-<p className="read-the-docs">
-  Click on the Vite and React logos to learn more
-</p> */}
